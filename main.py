811c07a5e9d0039d947165fd896c03dd6792f875
yuki <yk.me.firefox@gmail.com>
2023-06-09 11:18:03 +0900

    Dice game: initial revision

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+def dice():
+    print('Rolling dice...')
+    l = [1, 2, 3, 4, 5, 6]
+    sum = 0
+    for i in range(2):
+        r = random.choice(l)
+        print(f'Die {i}: {r}')
+        sum+=r
+    print(f'Total value: {sum}')
+
+if __name__ == '__main__':
+    dice()
